1ae8e9f19f6b9e98c4261ebf343d9297a0011eed
Max Gonzih <gonzih@gmail.com>
2020-01-10 12:41:08 -0500

    Small fixes for nixos

---
+++ shell.nix
@@ -0,0 +1,21 @@
+let
+  nixpkgs = import <nixpkgs> {};
+  inherit (nixpkgs) pkgs llvmPackages;
+  stdenv = pkgs.clangStdenv;
+in
+stdenv.mkDerivation {
+  name = "c2rust";
+  buildInputs = [
+    pkgs.python37Packages.rencode
+    pkgs.SDL2
+    pkgs.clang
+    pkgs.cmake
+    pkgs.llvm
+    pkgs.openssl
+    pkgs.pkgconfig
+    pkgs.python35
+    pkgs.rustup
+    pkgs.zlib
+  ];
+  LIBCLANG_PATH="${llvmPackages.libclang}/lib";
+}
